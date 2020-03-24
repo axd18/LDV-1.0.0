5ae10ac141bc33583767b49f232e5d490460672f
adrijfer18 <adrianfernandezj@gmail.com>
2020-03-24 14:07:14 -0300

    data reaches to db!

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -37,6 +37,15 @@ export class ContactComponent implements OnInit {
 
   onSaveForm() {
     console.log('saved');
+    const newContact = {
+      fname: 'adrian',
+      lname: 'fernandez',
+      email: 'adri@gmail.com',
+      subject: 'consulta',
+      message: '10% building 0/1 modules 1 active'
+    };
+
+    this.dbData.saveMessage(newContact);
   }
 
 }
