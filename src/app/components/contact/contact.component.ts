e56ec4901557bc5fefc576eba11e6ed656c2266e
adrijfer18 <adrianfernandezj@gmail.com>
2020-03-26 15:19:26 -0300

    Form validation ok and add sweet alert

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data-db.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
@@ -10,23 +11,33 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ContactComponent implements OnInit {
 
   contactForm: FormGroup;
+  Swal = 'sweetAlert';
 
+  // tslint:disable-next-line: max-line-length
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  constructor(private dbData: DataService) {
+    this.contactForm = this.createFormGroup();
+   }
+
+  get fname() { return this.contactForm.get('fname'); }
+  get lname() { return this.contactForm.get('lname'); }
+  get email() { return this.contactForm.get('email'); }
+  get message() { return this.contactForm.get('message'); }
 
   createFormGroup() {
     return new FormGroup({
-      fname: new FormControl(''),
-      lname: new FormControl(''),
-      email: new FormControl(''),
+      fname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      lname: new FormControl('', [Validators.minLength(4)]),
+      email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
       subject: new FormControl(''),
-      message: new FormControl(''),
+      message: new FormControl('', [Validators.required, Validators.minLength(10)]),
     });
   }
 
 
 
-  constructor(private dbData: DataService) {
-    this.contactForm = this.createFormGroup();
-   }
+
 
   ngOnInit(): void {
   }
@@ -36,8 +47,23 @@ export class ContactComponent implements OnInit {
   }
 
   onSaveForm() {
-    console.log('saved');
-    this.dbData.saveMessage(this.contactForm.value);
+    if (this.contactForm.valid) {
+      this.dbData.saveMessage(this.contactForm.value);
+      this.onResetForm();
+      console.log('Valid');
+    } else {
+      console.log('Not valid');
+    }
+  }
+
+  showModal() {
+    Swal.fire(
+      'Message sended!',
+      'Thank you! We will reply shortly.',
+      'success'
+    );
   }
 
 }
+
+
